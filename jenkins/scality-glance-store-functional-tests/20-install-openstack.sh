c0154a8d2fe93777cd061f2a57ee7772349ae17b
Jordan Pittier <jordan.pittier@scality.com>
2015-03-25 12:23:08 +0100

    Support OpenStack Juno

--- jenkins/scality-glance-store-functional-tests/20-install-openstack.sh
+++ jenkins/scality-glance-store-functional-tests/20-install-openstack.sh
@@ -9,11 +9,16 @@ function is_deb {
 }
 
 function common {
+    if is_centos; then
+        sudo yum install -y wget
+    fi
+
     wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python -
     sudo easy_install pip
+    sudo easy_install -U six
 
     if is_centos; then
-        sudo pip install -U six
+        sudo yum install -y https://kojipkgs.fedoraproject.org//packages/python-mox/0.5.3/2.el6/noarch/python-mox-0.5.3-2.el6.noarch.rpm
     fi
 
     git clone -b ${DEVSTACK_BRANCH:-master} https://github.com/openstack-dev/devstack.git
@@ -23,7 +28,8 @@ disable_service n-xvnc n-novnc n-obj n-cauth h-eng h-api h-api-cfn h-api-cw hori
 SCREEN_LOGDIR="\${DEST}/logs"
 EOF
     cp jenkins/${JOB_NAME%%/*}/extras.d/55-scality-glance-store.sh devstack/extras.d/55-scality-glance-store.sh
-    FORCE=yes ./devstack/stack.sh
+
+    ./devstack/stack.sh
 }
 
 common
