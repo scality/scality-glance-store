e38ea2a29bce4475bb12bc0e8880cbf57ccdecc8
Nicolas Trangez <ikke@nicolast.be>
2015-09-22 18:02:33 +0200

    CI: Hack - Apply Python2.6 patch locally

--- jenkins/scality-glance-store-functional-tests/30-run-tests.sh
+++ jenkins/scality-glance-store-functional-tests/30-run-tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -xue
 
 cd /opt/stack/tempest
+
+echo "*** Hack: Apply https://review.openstack.org/#/c/226375/"
+curl "https://review.openstack.org/gitweb?p=openstack/tempest.git;a=patch;h=edee6507df2b73ec76cccc37317581b1741fda2d" | git am
+
+git log -n2
+
 set +e
 tox -e all -- tempest.api.image
 RC=$?
