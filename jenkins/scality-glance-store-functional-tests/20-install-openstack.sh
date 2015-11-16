4b200cea48b3763e42f08dbdd9e34f91d4816548
Jordan Pittier <jordan.pittier@scality.com>
2015-11-16 14:19:47 +0100

    Rework the functional test job
    To make it simpler

--- jenkins/scality-glance-store-functional-tests/20-install-openstack.sh
+++ jenkins/scality-glance-store-functional-tests/20-install-openstack.sh
@@ -1,39 +1,39 @@
 #!/bin/bash -xue
 
-function common {
-    source jenkins/openstack-ci-scripts/jenkins/distro-utils.sh
+source jenkins/openstack-ci-scripts/jenkins/distro-utils.sh
 
-    if is_centos; then
-        sudo yum install -y wget
+if is_centos; then
+    sudo yum install -y ftp://ftp.is.co.za/mirror/fedora.redhat.com/epel/6/x86_64/python-mox-0.5.3-2.el6.noarch.rpm
+
+    if test "${DEVSTACK_BRANCH:-x}" = "stable/juno"; then
+        export RHEL6_RDO_REPO_RPM="http://buildlogs.centos.org/centos/6/cloud/x86_64/openstack-juno/centos-release-openstack-juno-2.el6.noarch.rpm"
+        export RHEL6_RDO_REPO_ID="CentOS-OpenStack-juno"
     fi
+fi
 
-    if [[ $os_CODENAME == "precise" ]]; then
-        sudo add-apt-repository --yes cloud-archive:icehouse
-    fi
+if [[ $os_CODENAME == "precise" ]]; then
+    sudo add-apt-repository --yes cloud-archive:icehouse
+fi
 
-    wget https://bootstrap.pypa.io/get-pip.py -O - | sudo python -
-    sudo pip install -U six
-
-    if is_centos; then
-        sudo yum install -y ftp://ftp.is.co.za/mirror/fedora.redhat.com/epel/6/x86_64/python-mox-0.5.3-2.el6.noarch.rpm
-
-        if test "${DEVSTACK_BRANCH:-x}" = "stable/juno"; then
-            RHEL6_RDO_REPO_RPM="http://buildlogs.centos.org/centos/6/cloud/x86_64/openstack-juno/centos-release-openstack-juno-2.el6.noarch.rpm"
-            export RHEL6_RDO_REPO_RPM
-            RHEL6_RDO_REPO_ID="CentOS-OpenStack-juno"
-            export RHEL6_RDO_REPO_ID
-        fi
-    fi
-
-    git clone -b ${DEVSTACK_BRANCH:-master} https://github.com/openstack-dev/devstack.git
-    cp devstack/samples/local.conf devstack/local.conf
-    cat >> devstack/local.conf <<EOF
-disable_service n-xvnc n-novnc n-obj n-cauth h-eng h-api h-api-cfn h-api-cw horizon
-SCREEN_LOGDIR="\${DEST}/logs"
+git clone -b ${DEVSTACK_BRANCH:-master} https://github.com/openstack-dev/devstack.git
+cat > devstack/local.conf <<-EOF
+	[[local|localrc]]
+	DATABASE_PASSWORD=testtest; RABBIT_PASSWORD=testtest; SERVICE_TOKEN=testtest; SERVICE_PASSWORD=testtest; ADMIN_PASSWORD=testtest; SCREEN_LOGDIR=\${DEST}/logs
+	disable_service n-xvnc n-novnc n-obj h-eng h-api h-api-cfn h-api-cw horizon
+	[[post-config|\$GLANCE_API_CONF]]
+	[glance_store]
+	default_store = scality
+	stores = glance.store.filesystem.Store,glance.store.http.Store,glance.store.scality.Store
+	scality_sproxyd_endpoints = http://127.0.0.1:81/proxy/bpchord
 EOF
-    cp jenkins/${JOB_NAME%%/*}/extras.d/55-scality-glance-store.sh devstack/extras.d/55-scality-glance-store.sh
 
-    ./devstack/stack.sh
-}
+cat > devstack/extras.d/55-scality-glance-store.sh <<-EOF
+	if is_service_enabled g-api; then
+	    if [[ "\$1" == "stack" && "\$2" == "install" ]]; then
+	        sudo pip install https://github.com/scality/scality-sproxyd-client/archive/master.tar.gz
+	        sudo pip install .
+	    fi
+	fi
+EOF
 
-common
+./devstack/stack.sh
--- jenkins/scality-glance-store-functional-tests/30-run-tests.sh
+++ jenkins/scality-glance-store-functional-tests/30-run-tests.sh
@@ -10,18 +10,10 @@ git log -n2
 set +e
 tox -e all -- tempest.api.image
 RC=$?
-testr last --subunit > $WORKSPACE/testrepository.subunit
 set -e
 
-echo "Entering WORKSPACE."
-cd $WORKSPACE
-mkdir jenkins-logs
-echo "Creating jenkins-log directory."
-cp -R /opt/stack/logs/* jenkins-logs/
-sudo chown jenkins jenkins-logs/*
-
-# Create a test result report
 sudo pip install junitxml
-cat ${WORKSPACE}/testrepository.subunit | subunit2junitxml -o ${WORKSPACE}/nosetests.xml
+# Create a test result report
+testr last --subunit | subunit2junitxml -o ${WORKSPACE}/nosetests.xml
 
 exit $RC
