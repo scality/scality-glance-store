c999072f98b24831a8f98bfe54f27e2b38c13147
Jordan Pittier <jordan.pittier@scality.com>
2016-04-28 11:04:44 +0200

    "Revert "CI: Hack - Apply Python2.6 patch locally"""
    This reverts commit 27e158f4254d1c367c6a19274d34fd4127049f2f.
    
    This commit [1], merged a couple of days ago, removed the
    invalid Py26 construct.
    
    [1] https://github.com/openstack/tempest/commit/27905cc79bc454a3f4cc39e21249b56ad40342a6

--- jenkins/scality-glance-store-functional-tests/30-run-tests.sh
+++ jenkins/scality-glance-store-functional-tests/30-run-tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -xue
 
 cd /opt/stack/tempest
-
-echo "*** Hack: Apply https://review.openstack.org/#/c/252837/"
-curl "https://review.openstack.org/gitweb?p=openstack/tempest.git;a=patch;h=660b440f12c607d720e59e1c6ab297c2c28b1ea3" | git am
-
-git log -n2
-
 set +e
 tox -e all -- tempest.api.image
 RC=$?
